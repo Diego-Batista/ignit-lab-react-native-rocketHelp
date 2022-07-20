65804c7afaccf6344006d08be71d3b49c32e6d8d
Diego-Batista <diegobtistadev@gmail.com>
2022-07-20 19:54:35 -0300

    segunda aula

--- App.tsx
+++ App.tsx
@@ -2,7 +2,10 @@ import { NativeBaseProvider, StatusBar } from "native-base";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import { THEME } from './src/styles/theme';
 import { SignIn } from './src/screens/SignIn';
+import { Home } from './src/screens/Home';
+import { Register } from './src/screens/Register';
 import { Loading } from "./src/components/Loading";
+import { Routes } from './src/routes'
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
@@ -10,7 +13,7 @@ export default function App() {
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
-      { fontsLoaded ? <SignIn/> : <Loading />}
+      { fontsLoaded ? <Routes/> : <Loading />}
     </NativeBaseProvider>
   );
 }
---
+++ src/screens/Details.tsx
@@ -0,0 +1,23 @@
+import { useRoute } from '@react-navigation/native';
+import { VStack, Text } from 'native-base';
+import { Header } from '../components/Header';
+import { Input } from '../components/Input';
+import { Button } from '../components/Button';
+
+type RouteParams = {
+  orderId: string
+}
+
+export function Details() {
+  const route = useRoute();
+
+  const { orderId } = route.params as RouteParams;
+  return (
+    <VStack flex={1} p={6} bg="gray.700">
+        <Header title="Solicitação"/>
+        <Text color="white">
+          {orderId}
+        </Text>
+    </VStack>
+  );
+}
